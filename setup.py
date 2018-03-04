2d4e6e578659fe650659da5383b374f82d753378
Michael Chase <michase@netflix.com>
2018-03-04 15:30:23 -0800

    add install_requires

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name='minemon',
     versioning='distance',          # Optional, would activate tag-based versioning
-    setup_requires='setupmeta'      # This is where setupmeta comes in
+    setup_requires='setupmeta'      # This is where setupmeta comes in,
+    install_requires=[
+        'requests',
+        'pytz',
+    ]
 )
